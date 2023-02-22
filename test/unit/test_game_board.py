14f536b1f68f6a2a59426ac3388391bb8d45bd1b
Matthew Isayan <misayan@gmail.com>
2023-02-22 13:12:06 -0800

    Adjusted test case to use factory instead

--- test/unit/test_game_board.py
+++ test/unit/test_game_board.py
@@ -1,13 +1,15 @@
 import pytest
 
 from inf122_tmge.model.board.game_board import GameBoard
+from inf122_tmge.core.board_factory import BoardFactory
 from inf122_tmge.model.tiles.tile import Tile
 
 def test_game_board_constructor():
     """Testing game_board's default constructor to ensure board is of proper height and width"""
     width = 10
     height = 24
-    board = GameBoard(width, height).board
+    game_board = BoardFactory.create_board("default", width, height)
+    board = game_board.board
 
     assert len(board) == width
     assert all(len(row) == height for row in board)
