7d67d728e4bc1f6252de17b7460420badbffc721
Matthew Isayan <misayan@gmail.com>
2023-02-22 12:52:04 -0800

    Moved the game_board test to another file

--- test/unit/test_inf122tmge.py
+++ test/unit/test_inf122tmge.py
@@ -1,8 +1,6 @@
 import pytest
 
 from inf122_tmge import gimme_five
-from inf122_tmge.model.board.game_board import GameBoard
-from inf122_tmge.model.tiles.tile import Tile
 
 def test_vacuous():
     """A test the passes vacuously"""
@@ -24,13 +22,3 @@ def test_exception_is_raised():
     """Test that an exception is raised"""
     with pytest.raises(ValueError):
         raise ValueError
-
-def test_game_board_constructor():
-    """Testing game_board's default constructor to ensure board is of proper height and width"""
-    width = 10
-    height = 24
-    board = GameBoard(width, height).board
-
-    assert len(board) == width
-    assert all(len(row) == height for row in board)
-    assert all(isinstance(tile, Tile) for row in board for tile in row)
