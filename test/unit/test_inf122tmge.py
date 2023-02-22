cf6b11f43368ea222086778441d31437bb9c90d2
Matthew Isayan <misayan@gmail.com>
2023-02-22 03:20:23 -0800

    Added test for game_board constructor

--- test/unit/test_inf122tmge.py
+++ test/unit/test_inf122tmge.py
@@ -1,6 +1,8 @@
 import pytest
 
 from inf122_tmge import gimme_five
+from inf122_tmge.model.board.game_board import GameBoard
+from inf122_tmge.model.tiles.tile import Tile
 
 def test_vacuous():
     """A test the passes vacuously"""
@@ -22,3 +24,13 @@ def test_exception_is_raised():
     """Test that an exception is raised"""
     with pytest.raises(ValueError):
         raise ValueError
+
+def test_game_board_constructor():
+    """Testing game_board's default constructor to ensure board is of proper height and width"""
+    width = 10
+    height = 24
+    board = GameBoard(width, height).board
+
+    assert len(board) == width
+    assert all(len(row) == height for row in board)
+    assert all(isinstance(tile, Tile) for row in board for tile in row)
