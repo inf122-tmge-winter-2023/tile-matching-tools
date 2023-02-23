e588c9120aed9acef0eba7d7627acf5a21837905
Matthew Isayan <misayan@gmail.com>
2023-02-23 00:43:37 -0800

    Tests for view class

---
+++ test/unit/test_view.py
@@ -0,0 +1,23 @@
+import pytest
+
+from inf122_tmge.core.board_factory import BoardFactory
+from inf122_tmge.model.tiles.tile import Tile
+from inf122_tmge.view.view import View
+
+# Mark test as integration to avoid executing with test suite
+@pytest.mark.integration
+def test_launch_view():
+    """Manual integration test to visually inspect the board"""
+    game_board = BoardFactory.create_board('default', 10, 24)
+    view = View(game_board) 
+    view.launch_view()
+
+
+@pytest.mark.integration
+def test_update_tile():
+    """Manual integration test to see if a tile is drawn"""
+    game_board = BoardFactory.create_board('default', 10, 24)
+    game_board.update_tile(3, 3, Tile('red'))
+    view = View(game_board) 
+
+    view.launch_view()
