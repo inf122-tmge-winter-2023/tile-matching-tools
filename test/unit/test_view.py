009e15434f23321a3beeb3d8a5ce5c566e035cd3
Matthew Isayan <misayan@gmail.com>
2023-02-24 14:39:13 -0800

    Testing colored tile on board

--- test/unit/test_view.py
+++ test/unit/test_view.py
@@ -1,6 +1,7 @@
 import pytest
 
 from inf122_tmge.core.board_factory import BoardFactory
+from inf122_tmge.core.tile_builder import TileBuilder
 from inf122_tmge.model.tiles.tile import Tile
 from inf122_tmge.view.view import View
 
@@ -13,11 +14,12 @@ def test_launch_view():
     view.launch_view()
 
 
-# @pytest.mark.integration
-# def test_update_tile():
-#     """Manual integration test to see if a tile is drawn"""
-#     game_board = BoardFactory.create_board('default', 10, 24)
-#     game_board.update_tile(3, 3, Tile(3,3,'red'))
-#     view = View(game_board) 
+@pytest.mark.integration
+def test_update_tile():
+    """Manual integration test to see if a tile is drawn"""
+    game_board = BoardFactory.create_board('default', 10, 24)
+    tile_to_place = TileBuilder().add_position(3,3).add_color('red').construct()
+    game_board.place_tile(tile_to_place, 3, 3)
+    view = View(game_board) 
 
-#     view.launch_view()
+    view.launch_view()
