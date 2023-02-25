bb77b43a1585197d073346f5415f3c7dafa1a63d
Matthew Isayan <misayan@gmail.com>
2023-02-24 17:29:44 -0800

    Threading proof of concept test

--- test/unit/test_view.py
+++ test/unit/test_view.py
@@ -1,3 +1,6 @@
+import copy
+from threading import Thread
+import time
 import pytest
 
 from inf122_tmge.core.board_factory import BoardFactory
@@ -22,4 +25,29 @@ def test_update_tile():
     game_board.place_tile(tile_to_place, 3, 3)
     view = View(game_board) 
 
+    view.launch_view()
+
+@pytest.mark.integration
+def test_threading_fill_board():
+    """Manual integration test to see board is filled"""
+    game_board = BoardFactory.create_board('default', 10, 24)
+    view = View(game_board) 
+
+    def place_thread():
+        t1=Thread(target=place_tiles, daemon=True)
+        t1.start()
+  
+    def place_tiles():
+        for i in range(1,11):
+
+            for j in range(1,25):
+                tile_to_place = TileBuilder().add_position(i,j).add_color('red').construct()
+                board2 = copy.deepcopy(game_board)
+
+                board2.place_tile(tile_to_place, i, j)
+                view.update_board(board2)
+                time.sleep(.01667)
+    
+    # After 1000 ms execute the thread
+    view.root.after(1000, place_thread)
     view.launch_view()
