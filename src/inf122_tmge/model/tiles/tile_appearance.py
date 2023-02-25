2ba9a5c72a2a7175c05a58a38b154b59e90919e9
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2023-02-25 15:18:27 -0800

    Add default set of shapes and colors for tile appearance

--- src/inf122_tmge/model/tiles/tile_appearance.py
+++ src/inf122_tmge/model/tiles/tile_appearance.py
@@ -5,6 +5,30 @@
 """
 
 from abc import ABC
+from dataclasses import dataclass
+from enum import StrEnum, IntEnum
 
+class TileColor(StrEnum):
+    """
+        Class the enumerates possible default set of colors for tile appearance
+    """
+    RED = '#FF0000'
+    ORANGE = '#FF7F00'
+    YELLOW = '#FFFF00'
+    GREEN = '#00FF00'
+    BLUE = '#0000FF'
+    INDIGO = '#4B0082'
+    VIOLET = '#9400D3'
+
+class TileShape(IntEnum):
+    """
+        Class that enumerates possible default set of shapes for tile appearance
+    """
+    SQUARE = 1
+    DIAMOND = 2
+    CIRCULAR = 3
+
+@dataclass
 class TileAppearance(ABC):
-    pass
+    color: TileColor
+    shape: TileShape
