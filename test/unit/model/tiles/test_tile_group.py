041c97436c165b9ea3ca01ab2ff9f48c38fce7d4
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2023-03-05 14:02:48 -0800

    Add tests for tile group movements

--- test/unit/model/tiles/test_tile_group.py
+++ test/unit/model/tiles/test_tile_group.py
@@ -2,7 +2,7 @@
 
 import pytest
 
-from inf122_tmge.model import TileGroup
+from inf122_tmge.model import TileGroup, MovementRule
 from inf122_tmge.model.exceptions import TileGroupPositionOccupiedError, \
                                          TileGroupDisbandedException
 from inf122_tmge.core import TileBuilder
@@ -40,3 +40,20 @@ class TestTileGroup:
                     1,
                     1
                     )
+
+    def test_movement_rule_applies_to_all_tiles_in_group(self):
+        self._group.add_sibling_tile(
+                self._builder.add_position(5, 5).construct(),
+                0,
+                -1
+                )
+        class MoveDown(MovementRule):
+            def exec(self, x, y):
+                return (x + self._dx, y + self._dy)
+
+        self._group.move(MoveDown(0, -1))
+        assert self._group._tiles[(0, 0)].position.x == 5
+        assert self._group._tiles[(0, 0)].position.y == 4
+
+        assert self._group._tiles[(0, -1)].position.x == 5
+        assert self._group._tiles[(0, -1)].position.y == 3
