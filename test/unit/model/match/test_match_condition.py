656409236398ded80cc343d1d63fb2651470ca8e
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2023-03-05 15:32:34 -0800

    Add tests for match condition interface

---
+++ test/unit/model/match/test_match_condition.py
@@ -0,0 +1,48 @@
+"""Tests for match condition"""
+
+import pytest
+
+from inf122_tmge.model.match import MatchCondition
+from inf122_tmge.model import GameBoard
+from inf122_tmge.model.exceptions import InvalidBoardPositionError
+from inf122_tmge.model.tiles import Tile
+
+@pytest.fixture
+def two_match():
+    class TwoMatch(MatchCondition):
+        def check_match(self, board, start_x, start_y):
+            try:
+                return self._eq(
+                    board.tile_at(start_x, start_y),
+                    board.tile_at(
+                        start_x + self._scan_delta[0],
+                        start_y + self._scan_delta[1]
+                            )
+                    )
+            except InvalidBoardPositionError:
+                return False
+    return TwoMatch((0, -1))
+
+
+def test_match_condition_must_implement_the_match_interface():
+    with pytest.raises(TypeError):
+        m = MatchCondition((0, 1), lambda: False)
+
+    with pytest.raises(TypeError):
+        class TestCondition(MatchCondition):
+            def i_dont_implement_the_match_interface():
+                pass
+        m = TestCondition((0, 1), lambda: False)
+
+class TestMatchOnBoard:
+    def setup_method(self):
+        self._board = GameBoard(3, 3)
+        self._board.place_tile(Tile(**{'position': (1, 1)}), 1, 1)
+        self._board.place_tile(Tile(**{'position': (1, 2)}), 1, 2)
+        self._board.place_tile(Tile(**{'position': (2, 2)}), 2, 2)
+
+    def test_can_detect_a_match(self, two_match):
+        assert two_match.check_match(self._board, 1, 2)
+
+    def test_can_detect_a_nonmatch(self, two_match):
+        assert not two_match.check_match(self._board, 2, 2)
