2c0fce599963243f2a79a707c1d92d9359ed7f06
Matthew <misayan@gmail.com>
2023-03-07 20:14:57 -0800

    Game State dataclass

--- src/inf122_tmge/core/game_state.py
+++ src/inf122_tmge/core/game_state.py
@@ -1,10 +1,18 @@
 """
     :module_name: game_state
     :module_summary: an extensible class capable of representing the state of a tile-matching game
-    :module_author: Nathan Mendoza (nathancm@uci.edu)
+    :module_author: Nathan Mendoza (nathancm@uci.edu), Matthew Isayan
 """
 
-from abc import ABC
+from dataclasses import dataclass
 
-class GameState(ABC):
-    pass
+from inf122_tmge.model.board.game_board import GameBoard
+from inf122_tmge.model.score import Scoring
+
+@dataclass
+class GameState:
+    """
+        Class responsible for holding the gameboard and score 
+    """
+    game_board: GameBoard
+    score: Scoring
