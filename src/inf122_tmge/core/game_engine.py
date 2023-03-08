ac5d9a353b131a8d00a0429da1fddc3c6eb381dd
Matthew <misayan@gmail.com>
2023-03-07 23:59:05 -0800

    Propogated methods

--- src/inf122_tmge/core/game_engine.py
+++ src/inf122_tmge/core/game_engine.py
@@ -13,6 +13,7 @@ from inf122_tmge.model import Scoring
 from inf122_tmge.model import MatchCondition
 
 from inf122_tmge.model import MovementRule
+from inf122_tmge.model import Tile
 
 class GameEngine(ABC):
     def __init__(self, board: GameBoard, score : Scoring):
@@ -21,11 +22,61 @@ class GameEngine(ABC):
     # TODO: Replace with updated place_tile
     # TODO: Handle exceptions, possibly chain exceptions
     def move_tile(self, row: int, col: int, rule: MovementRule):
-        tile_to_move = self.game_state.game_board.tile_at(row, col)
+        """Applies movement rule to tile at (row, col)
+
+        Args:
+            row (int): row of tile
+            col (int): col of tile
+            rule (MovementRule): Concrete MovementRule
+        """
+        tile_to_move = self.tile_at(row, col)
         tile_to_move.move(rule)
-        self.game_state.game_board.place_tile(tile_to_move, tile_to_move.position.x, tile_to_move.position.y)
+        self.place_tile(tile_to_move, tile_to_move.position.x, tile_to_move.position.y)
 
     # TODO Implement aftermath of a match
     def match_tiles(self, start_x: int, start_y: int, match_condition: MatchCondition):
+        """Checks if tiles match, then awards for match accordingly
+
+        Args:
+            start_x (int): the x position the match scans for
+            start_y (int): the y position the match scans for
+            match_condition (MatchCondition): the match condition that awards points
+        """
         if match_condition.check_match(self.game_state.game_board,start_x, start_y):
             self.game_state.game_score.award_for_match(match_condition)
+    
+    # TODO: Replace with updated place_tile
+    def place_tile(self, tile: Tile, row: int, col: int):
+        """Propogated place_tile from game_board 
+
+        Args:
+            tile (Tile): tile to place
+            row (int): row of the tile
+            col (int): col of the tile
+        """
+        self.game_state.game_board.place_tile(tile, row, col)
+
+    def tile_at(self, row: int, col: int) -> Tile:
+        """Propogated tile_at from game_board 
+
+        Args:
+            row (int): row of the tile
+            col (int): col of the tile
+
+        Returns:
+            Tile: Tile at (row, col) position
+
+        Raises:
+            InvalidBoardPosition for invalid tile positions
+        """
+        return self.game_state.game_board.tile_at(row, col)
+    
+    @property
+    def score(self) -> int:
+        """Propogated score property from game_score
+
+        Returns:
+            int: score
+        """
+        return self.game_state.game_score.score
+    
