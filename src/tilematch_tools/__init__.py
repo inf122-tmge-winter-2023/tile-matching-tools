cc3e2ee14f9fe715bb3e7b23a6c5fa68e31446d0
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2023-03-08 23:09:47 -0800

    Add package-level logging facilities

--- src/tilematch_tools/__init__.py
+++ src/tilematch_tools/__init__.py
@@ -1,11 +1,23 @@
 """
-    Dummy module
+    :module_name: tilematch_tools
+    :module_summary: entry point to the tilematch_tools package
+    :module_author: Nathan Mendoza (nathancm@uci.edu)
 """
+
+import logging
+
+LOGGER = logging.getLogger(__name__)
+LOG_HANDLER = logging.StreamHandler()
+LOG_FORMAT = logging.Formatter('[%(asctime)s|%(name)s|%(levelname)s] - %(message)s')
+
+LOGGER.setLevel(logging.DEBUG)
+LOG_HANDLER.setLevel(logging.DEBUG)
+
+LOG_HANDLER.setFormatter(LOG_FORMAT)
+LOGGER.addHandler(LOG_HANDLER)
+
+__version__ = (0, 0, 1)
 
 from .core import *
 from .model import *
 from .view import *
-
-def gimme_five():
-    """A dumb function that always returns 5"""
-    return 5
