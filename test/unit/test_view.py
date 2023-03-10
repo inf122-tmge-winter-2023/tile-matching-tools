6e2dd569c4a7e7a7d0425215747da1ea442d111c
Matthew <misayan@gmail.com>
2023-03-10 14:26:15 -0800

    Removed Old tests

--- test/unit/test_view.py
+++ test/unit/test_view.py
@@ -14,41 +14,6 @@ def setup_function():
     LOG_HANDLER.setLevel(51)
     LOGGER.setLevel(51)
 
-# Mark test as integration to avoid executing with test suite
-@pytest.mark.skip
-def test_launch_view():
-    """Manual integration test to visually inspect the board"""
-    game_board = BoardFactory.create_board('default', 10, 24)
-    view = View(game_board) 
-    view.launch_view()
-
-
-@pytest.mark.skip
-def test_update_tile():
-    """Manual integration test to see if a tile is drawn"""
-    game_board = BoardFactory.create_board('default', 10, 24)
-    tile_to_place = TileBuilder().add_position(3,3).add_color('red').construct()
-    game_board.place_tile(tile_to_place)
-    view = View(game_board) 
-    view.launch_view()
-
-@pytest.mark.skip
-def test_threading_fill_board():
-    """Manual integration test to see board is filled one by one"""
-    game_board = BoardFactory.create_board('default', 10, 24)
-    view = View(game_board) 
-
-    def place_tiles():
-        for i in range(1,11):
-            for j in range(1,25):
-                tile_to_place = TileBuilder().add_position(i,j).add_color('red').construct()
-
-                game_board.place_tile(tile_to_place)
-                view._update_board_view(game_board)
-                time.sleep(.0165)
-
-    view.launch_view(place_tiles)
-
 @pytest.mark.integration
 def test_user_input():
     """Manual integration testing a user input"""
