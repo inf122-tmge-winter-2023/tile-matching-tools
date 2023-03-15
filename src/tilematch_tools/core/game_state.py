d9bcca719f75be3add1c1ba331d7263bd0cc34b2
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2023-03-15 00:04:05 -0700

    Extend current game state to handle some things from game engine while game loop handles the rest

--- src/tilematch_tools/core/game_state.py
+++ src/tilematch_tools/core/game_state.py
@@ -5,17 +5,32 @@
 """
 
 import logging
-from dataclasses import dataclass
 
-from ..model import GameBoard
-from ..model import Scoring
+from ..model import GameBoard, Scoring, MatchCondition
 
 LOGGER = logging.getLogger(__name__)
 
-@dataclass
 class GameState:
     """
         Class responsible for holding the gameboard and score 
     """
-    game_board: GameBoard
-    game_score: Scoring
+    def __init__(self, board: GameBoard, score: Scoring):
+        self._board = board
+        self._score = score
+        self._match_conditions = []
+
+
+
+    def add_match_condition(self, match_cond):
+        """
+            Adds a match conditions that can affect this game state
+            :arg match_cond: the new match condition
+            :arg type: MatchCondition
+            :returns: nothing
+            :rtype: None
+        """
+        self._match_conditions.append(match_cond)
+
+    @property
+    def match_rules(self):
+        return self._match_conditions
