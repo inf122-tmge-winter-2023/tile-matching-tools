f43e2de1b5be5cdd4497bc911f2ab6a00ac81417
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2023-03-15 10:09:22 -0700

    Game engine tests now focus on game state responsabilities

--- test/unit/core/test_game_state.py
+++ test/unit/core/test_game_state.py
@@ -1,5 +1,5 @@
 import pytest
-from tilematch_tools.core import GameEngine, BoardFactory, TileBuilder
+from tilematch_tools.core import GameState, BoardFactory, TileBuilder
 from tilematch_tools.model import Scoring, MovementRule, MatchCondition, TileColor, GameBoard
 from tilematch_tools.model.exceptions import IllegalTileMovementException, InvalidBoardPositionError
 from tilematch_tools.model.tiles.tile import NullTile
@@ -38,72 +38,80 @@ def two_match():
     return TwoMatch((0, -1), 4)
 
 
-@pytest.fixture
-def simple_score():
-    class SimpleScore(Scoring):
-        def __init__(self):
-            super().__init__()
+class SimpleScore(Scoring):
+    def __init__(self):
+        super().__init__()
 
-        def award_for_match(self, match):
-            self._points += 4
+    def award_for_match(self, match):
+        self._points += 4
 
-    return SimpleScore()
 
-class TestGameEngine:
-    def test_move_tile(self, simple_score, simple_down_movement):
+class TestGameState:
+    
+    def setup_method(self, simple_score):
+        self.state = GameState(
+                BoardFactory.create_board(GameBoard, 10, 24),
+                SimpleScore()
+                )
+
+    def test_move_tile(self, simple_down_movement):
         """
             Testing tile movement
         """
-        game_engine = GameEngine(BoardFactory.create_board(GameBoard, 10, 24), simple_score)
 
         test_tile = TileBuilder().add_position(1,3).add_color(TileColor.RED).construct()
-        game_engine.game_state.game_board.place_tile(test_tile)
-        game_engine.move_tile(test_tile, simple_down_movement)
-        assert game_engine.tile_at(1,4) == test_tile
+        self.state.board.place_tile(test_tile)
+        self.state.move_tile(test_tile, simple_down_movement)
+        assert self.state.board.tile_at(1,4) == test_tile
     
-    def test_cant_move_immovable(self, simple_down_movement, simple_score):
-        game_engine = GameEngine(BoardFactory.create_board(GameBoard, 10, 24), simple_score)
-
+    def test_cant_move_immovable(self, simple_down_movement):
         test_tile = TileBuilder().add_position(1,3).add_color(TileColor.RED).construct()
         test_tile._movable = False
-        game_engine.game_state.game_board.place_tile(test_tile)
-        game_engine.move_tile(test_tile, simple_down_movement)
+        self.state.board.place_tile(test_tile)
+        self.state.move_tile(test_tile, simple_down_movement)
         assert test_tile.position.y == 3
 
-    def test_match_tiles(self, simple_score, two_match):
-        """
-            Testing match tile updates score
-        """
-        game_engine = GameEngine(BoardFactory.create_board(GameBoard, 10, 24), simple_score)
+
+    def test_find_match(self, two_match):
         tile_1 = TileBuilder().add_position(1,3).add_color(TileColor.RED).construct()
         tile_2 = TileBuilder().add_position(1,2).add_color(TileColor.RED).construct()
-        game_engine.place_tile(tile_1)
-        game_engine.place_tile(tile_2)
-        game_engine.match_tiles(1,3,two_match)
-        assert game_engine.score == 4
-        assert isinstance(game_engine.tile_at(1, 3), NullTile)
-        assert isinstance(game_engine.tile_at(1, 2), NullTile)
+        self.state.board.place_tile(tile_1)
+        self.state.board.place_tile(tile_2)
+        assert self.state.find_match(1, 3, two_match)
 
-    def test_tiles_dont_match(self, simple_score, two_match):
+    def test_handle_match(self, two_match):
         """
             Testing match tile updates score
         """
-        game_engine = GameEngine(BoardFactory.create_board(GameBoard, 10, 24), simple_score)
+        tile_1 = TileBuilder().add_position(1,3).add_color(TileColor.RED).construct()
+        tile_2 = TileBuilder().add_position(1,2).add_color(TileColor.RED).construct()
+        self.state.board.place_tile(tile_1)
+        self.state.board.place_tile(tile_2)
+        match = self.state.find_match(1,3,two_match)
+        self.state.clear_match(match)
+        self.state.adjust_score(match)
+        assert self.state.score == 4
+        assert isinstance(self.state.board.tile_at(1, 3), NullTile)
+        assert isinstance(self.state.board.tile_at(1, 2), NullTile)
+
+    def test_tiles_dont_match(self, two_match):
+        """
+            Testing match tile updates score
+        """
         tile_1 = TileBuilder().add_position(5,3).add_color(TileColor.RED).construct()
         tile_2 = TileBuilder().add_position(1,2).add_color(TileColor.RED).construct()
-        game_engine.place_tile(tile_1)
-        game_engine.place_tile(tile_2)
+        self.state.board.place_tile(tile_1)
+        self.state.board.place_tile(tile_2)
         
-        assert game_engine.match_tiles(1,3,two_match) == False
+        assert self.state.find_match(1,3,two_match) is None
 
-    def test_swap_tiles(self, simple_score):
-        game_engine = GameEngine(BoardFactory.create_board(GameBoard, 10, 24), simple_score)
+    def test_swap_tiles(self):
         tile_1 = TileBuilder().add_position(1,3).add_color(TileColor.BLUE).construct()
         tile_2 = TileBuilder().add_position(1,2).add_color(TileColor.RED).construct()
-        game_engine.place_tile(tile_1)
-        game_engine.place_tile(tile_2)
-        game_engine.swap_tiles(tile_1, tile_2)
+        self.state.board.place_tile(tile_1)
+        self.state.board.place_tile(tile_2)
+        self.state.swap_tiles(tile_1, tile_2)
         assert tile_1.position.y == 2
         assert tile_2.position.y == 3
-        assert game_engine.tile_at(1,3).color == TileColor.RED
-        assert game_engine.tile_at(1,2).color == TileColor.BLUE
+        assert self.state.board.tile_at(1,3).color == TileColor.RED
+        assert self.state.board.tile_at(1,2).color == TileColor.BLUE
