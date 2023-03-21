d7c8d66c0c7c17359586b7bad7c31fd3a6c42791
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2023-03-21 09:18:34 -0700

    Fix score manipulation test

--- test/unit/view/test_score_view.py
+++ test/unit/view/test_score_view.py
@@ -12,6 +12,8 @@ def simple_score():
         def award_for_match(self, match):
             super().award_for_match(match)
 
+    return MyScore()
+
 @pytest.mark.integration
 def test_simple_score_display(simple_score):
     root = tk.Tk()
@@ -22,19 +24,22 @@ def test_simple_score_display(simple_score):
 @pytest.mark.integration
 def test_simple_score_update(simple_score):
     def add_score(event):
+        print('10 points earned')
         simple_score.award_for_match(MatchCondition.MatchFound(10, []))
 
     def lose_score(event):
+        print('10 points lost')
         simple_score.award_for_match(MatchCondition.MatchFound(-10, []))
 
 
     root = tk.Tk()
-    score_view = ScoreView(root, simple_score)
+    score_view = ScoreView(root, score_to_watch=simple_score)
     score_view.pack()
-    score_view.bind_all('<KeyRelease-a>', add_score)
-    score_view.bind_all('<KeyRelease-l>', lose_score)
+    root.bind_all('<KeyRelease-a>', add_score)
+    root.bind_all('<KeyRelease-l>', lose_score)
 
     def update_score():
+        print(f'Updating score to {simple_score.score}')
         score_view.update_score_display()
         score_view.after(100, update_score)
 
