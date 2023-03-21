892381e9d4bc36a5a3991841d93932acda610c99
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2023-03-21 00:01:11 -0700

    Add tests for updating score view labels

--- test/unit/view/test_score_view.py
+++ test/unit/view/test_score_view.py
@@ -4,7 +4,7 @@ import tkinter as tk
 
 import pytest
 
-from tilematch_tools import Scoring, ScoreView
+from tilematch_tools import MatchCondition, Scoring, ScoreView
 
 @pytest.fixture
 def simple_score():
@@ -13,7 +13,31 @@ def simple_score():
             super().award_for_match(match)
 
 @pytest.mark.integration
-def simple_score_display(simple_score):
+def test_simple_score_display(simple_score):
     root = tk.Tk()
     score_view = ScoreView(root, simple_score)
     score_view.pack()
+    root.mainloop()
+
+@pytest.mark.integration
+def test_simple_score_update(simple_score):
+    def add_score(event):
+        simple_score.award_for_match(MatchCondition.MatchFound(10, []))
+
+    def lose_score(event):
+        simple_score.award_for_match(MatchCondition.MatchFound(-10, []))
+
+
+    root = tk.Tk()
+    score_view = ScoreView(root, simple_score)
+    score_view.pack()
+    score_view.bind_all('<KeyRelease-a>', add_score)
+    score_view.bind_all('<KeyRelease-l>', lose_score)
+
+    def update_score():
+        score_view.update_score_display()
+        score_view.after(100, update_score)
+
+    score_view.after(100, update_score)
+    root.mainloop()
+
