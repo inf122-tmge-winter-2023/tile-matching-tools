946defb8a1d13f754346a0622ff4b627c88980f9
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2023-03-21 18:59:20 -0700

    Add test for independent binds

--- test/unit/view/test_game_view.py
+++ test/unit/view/test_game_view.py
@@ -100,3 +100,82 @@ def test_game_view_update_cycle(simple_game_state, move_rule_up, move_rule_down,
     root.after(100, update)
     root.mainloop()
 
+@pytest.mark.integration
+def test_independent_game_view_bindings(move_rule_up, move_rule_down, move_rule_left, move_rule_right):
+    gs1 = GameState(
+        BoardFactory.create_board(GameBoard, 10, 24),
+        Scoring()
+    )
+    gs2 = GameState(
+        BoardFactory.create_board(GameBoard, 10, 24),
+        Scoring()
+    )
+    moving_tile1 = TileBuilder() \
+            .add_position(random.randint(1, gs1.board.num_cols), random.randint(1, gs1.board.num_rows)) \
+            .add_color(random.choice(list(TileColor))) \
+            .construct()
+    moving_tile2 = TileBuilder() \
+            .add_position(random.randint(1, gs2.board.num_cols), random.randint(1, gs2.board.num_rows)) \
+            .add_color(random.choice(list(TileColor))) \
+            .construct()
+
+    gs1.board.place_tile(moving_tile1)
+    gs2.board.place_tile(moving_tile2)
+
+
+    root = tk.Tk()
+    gv1 = GameView(root, gs1)
+    gv2 = GameView(root, gs2)
+    gv1.grid(row=0, column=0)
+    gv2.grid(row=0, column=1)
+    
+    def move_up1(event):
+        move_rule_up.move(gs1.board, moving_tile1)
+        gs1.adjust_score(MatchCondition.MatchFound(random.randint(10, 50), []))
+
+    def move_dn1(event):
+        move_rule_down.move(gs1.board, moving_tile1)
+        gs1.adjust_score(MatchCondition.MatchFound(random.randint(10, 50), []))
+
+    def move_left1(event):
+        move_rule_left.move(gs1.board, moving_tile1)
+        gs1.adjust_score(MatchCondition.MatchFound(random.randint(10, 50), []))
+
+    def move_right1(event):
+        move_rule_right.move(gs1.board, moving_tile1)
+        gs1.adjust_score(MatchCondition.MatchFound(random.randint(10, 50), []))
+
+    gv1.bind_all('<KeyRelease-w>', move_up1)
+    gv1.bind_all('<KeyRelease-s>', move_dn1)
+    gv1.bind_all('<KeyRelease-a>', move_left1)
+    gv1.bind_all('<KeyRelease-d>', move_right1)
+
+    def move_up2(event):
+        move_rule_up.move(gs2.board, moving_tile2)
+        gs2.adjust_score(MatchCondition.MatchFound(random.randint(10, 50), []))
+
+    def move_dn2(event):
+        move_rule_down.move(gs2.board, moving_tile2)
+        gs2.adjust_score(MatchCondition.MatchFound(random.randint(10, 50), []))
+
+    def move_left2(event):
+        move_rule_left.move(gs2.board, moving_tile2)
+        gs2.adjust_score(MatchCondition.MatchFound(random.randint(10, 50), []))
+
+    def move_right2(event):
+        move_rule_right.move(gs2.board, moving_tile2)
+        gs2.adjust_score(MatchCondition.MatchFound(random.randint(10, 50), []))
+
+    gv2.bind_all('<KeyRelease-i>', move_up2)
+    gv2.bind_all('<KeyRelease-k>', move_dn2)
+    gv2.bind_all('<KeyRelease-j>', move_left2)
+    gv2.bind_all('<KeyRelease-l>', move_right2)
+
+    def update():
+        gv1.update()
+        gv2.update()
+        root.after(100, update)
+
+    root.after(100, update)
+    root.mainloop()
+
