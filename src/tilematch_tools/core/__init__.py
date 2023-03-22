f56e895475ff88609107bbcefeb09b7c5c028c80
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2023-03-21 23:03:30 -0700

    Add game dataclass to core importables

--- src/tilematch_tools/core/__init__.py
+++ src/tilematch_tools/core/__init__.py
@@ -8,4 +8,4 @@ from .tile_builder import TileBuilder
 from .board_factory import BoardFactory
 from .game_state import GameState
 from .game_loop import GameLoop
-from .game_engine import GameEngine
+from .game_engine import GameEngine, Game
