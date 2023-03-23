95fdd05c03271f2dd9836d6f51f00c50da98f00b
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2023-03-22 20:19:28 -0700

    Fix game title usage

--- src/tilematch_tools/view/__init__.py
+++ src/tilematch_tools/view/__init__.py
@@ -9,3 +9,4 @@ from .game_widgets import GameWidget, GameInfo
 from .score_view import ScoreView
 from .board_view import BoardView
 from .game_view import GameView
+from .game_title import GameTitle
